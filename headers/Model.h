65c2a932c54b791b216da4cdd351fbc7d012bd42
diogogomesaraujo <118282799+diogogomesaraujo@users.noreply.github.com>
2025-01-04 02:48:09 +0000

    my changes

--- headers/Model.h
+++ headers/Model.h
@@ -1,113 +1,52 @@
-#pragma once
-
 #include "header.h"
 #include "Mesh.h"
-
-#include <unordered_map>
-#include <tuple>
-#include <string>
-#include <iostream>
-#include <vector>
-#include <cmath>
-
-// Include Assimp
-#include <assimp/Importer.hpp>
-#include <assimp/scene.h>
-#include <assimp/postprocess.h>
-
-// Include stb_image if you haven't already
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
-
-using std::vector;
-using std::tuple;
-using std::string;
-using std::make_tuple;
-using std::abs;
-using std::get;
-using std::cout;
-using std::endl;
 
 class Model
 {
 public:
-    // Position of the model in the scene
-    tuple<float, float, float> position = make_tuple(0.0f, 0.0f, 0.0f);
+    vector<Texture> textures_loaded;
+    tuple<float, float, float> position;
 
-    // A static texture cache to avoid loading the same texture multiple times
-    static std::unordered_map<std::string, Texture> textureCache;
-
-    // Constructor: automatically loads a model from a file
     Model(const char *path)
     {
         loadModel(path);
     }
-
-    // Render the model using a given shader
     void Draw(Shader &shader)
     {
         for (unsigned int i = 0; i < meshes.size(); i++)
-        {
             meshes[i].Draw(shader);
-        }
     }
 
-    /**
-     *  The new lerp function:
-     *    - Moves 'a' towards 'b' by step 's'
-     *    - If the distance is less than 's', it returns 'b'
-     */
-    float lerp(float a, float b, float s)
+    float lerp(float a, float b, float f)
     {
-        float distance = std::fabs(a - b);
-        if (distance < s)
-            return b;
-        else if (a < b)
-            return a + s;
-        else
-            return a - s;
+        return a * (1.0 - f) + (b * f);
     }
 
-    // Moves the model's position gradually toward finalPos
     void modelMove(tuple<float, float, float> finalPos)
     {
-        // Use 'speed' as a step, not as a fraction
-        float speed = 0.5f; 
-        position = make_tuple(
-            lerp(get<0>(position), get<0>(finalPos), speed),
-            lerp(get<1>(position), get<1>(finalPos), speed),
-            lerp(get<2>(position), get<2>(finalPos), speed)
-        );
+        float speed = 0.005f;
+
+        position = make_tuple(lerp(get<0>(position), get<0>(finalPos), speed), lerp(get<1>(position), get<1>(finalPos), speed), lerp(get<2>(position), get<2>(finalPos), speed));
     }
 
-    // Returns true if the model is "close enough" to the target position
     bool hasReachedTarget(tuple<float, float, float> finalPos)
     {
-        float threshold = 3.0f; // Acceptable distance to the target
-        return (std::fabs(get<0>(position) - get<0>(finalPos)) < threshold &&
-                std::fabs(get<1>(position) - get<1>(finalPos)) < threshold &&
-                std::fabs(get<2>(position) - get<2>(finalPos)) < threshold);
+        float threshold = 3.0f;
+
+         return (abs(get<0>(position) - get<0>(finalPos)) < threshold &&
+            abs(get<1>(position) - get<1>(finalPos)) < threshold &&
+            abs(get<2>(position) - get<2>(finalPos)) < threshold);
     }
 
 private:
-    // The collection of meshes that compose this model
+    // model data
     vector<Mesh> meshes;
-    // The directory (folder) where the model file is located
     string directory;
 
-    // Loads a model from file using Assimp
-    void loadModel(const string &path)
+    void loadModel(string path)
     {
         Assimp::Importer import;
-        // Add extra optimization flags
-        const aiScene *scene = import.ReadFile(
-            path,
-            aiProcess_Triangulate         |
-            aiProcess_FlipUVs             |
-            aiProcess_OptimizeMeshes      |
-            aiProcess_OptimizeGraph       |
-            aiProcess_JoinIdenticalVertices
-        );
+        const aiScene *scene = import.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs);
 
         if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode)
         {
@@ -116,138 +55,128 @@ private:
         }
         directory = path.substr(0, path.find_last_of('/'));
 
-        // Process the root node recursively
         processNode(scene->mRootNode, scene);
     }
 
-    // Recursively processes a node in the Assimp scene
     void processNode(aiNode *node, const aiScene *scene)
     {
-        // Process each mesh at this node
+        // process all the node's meshes (if any)
         for (unsigned int i = 0; i < node->mNumMeshes; i++)
         {
             aiMesh *mesh = scene->mMeshes[node->mMeshes[i]];
             meshes.push_back(processMesh(mesh, scene));
         }
-        // Recursively process child nodes
+        // then do the same for each of its children
         for (unsigned int i = 0; i < node->mNumChildren; i++)
         {
             processNode(node->mChildren[i], scene);
         }
     }
 
-    // Extract mesh data from an Assimp mesh
     Mesh processMesh(aiMesh *mesh, const aiScene *scene)
     {
+        // data to fill
         vector<Vertex> vertices;
         vector<unsigned int> indices;
         vector<Texture> textures;
 
-        // Walk through each vertex
+        // walk through each of the mesh's vertices
         for (unsigned int i = 0; i < mesh->mNumVertices; i++)
         {
             Vertex vertex;
-            glm::vec3 vector3; 
-
-            // Positions
-            vector3.x = mesh->mVertices[i].x;
-            vector3.y = mesh->mVertices[i].y;
-            vector3.z = mesh->mVertices[i].z;
-            vertex.Position = vector3;
-
-            // Normals
+            glm::vec3 vector; // we declare a placeholder vector since assimp uses its own vector class that doesn't directly convert to glm's vec3 class so we transfer the data to this placeholder glm::vec3 first.
+            // positions
+            vector.x = mesh->mVertices[i].x;
+            vector.y = mesh->mVertices[i].y;
+            vector.z = mesh->mVertices[i].z;
+            vertex.Position = vector;
+            // normals
             if (mesh->HasNormals())
             {
-                vector3.x = mesh->mNormals[i].x;
-                vector3.y = mesh->mNormals[i].y;
-                vector3.z = mesh->mNormals[i].z;
-                vertex.Normal = vector3;
+                vector.x = mesh->mNormals[i].x;
+                vector.y = mesh->mNormals[i].y;
+                vector.z = mesh->mNormals[i].z;
+                vertex.Normal = vector;
+            }
+            // texture coordinates
+            if (mesh->mTextureCoords[0]) // does the mesh contain texture coordinates?
+            {
+                glm::vec2 vec;
+                // a vertex can contain up to 8 different texture coordinates. We thus make the assumption that we won't
+                // use models where a vertex can have multiple texture coordinates so we always take the first set (0).
+                vec.x = mesh->mTextureCoords[0][i].x;
+                vec.y = mesh->mTextureCoords[0][i].y;
+                vertex.TexCoords = vec;
             }
             else
-            {
-                vertex.Normal = glm::vec3(0.0f, 0.0f, 0.0f);
-            }
-
-            // Texture Coordinates
-            if (mesh->mTextureCoords[0])
-            {
-                glm::vec2 vec2;
-                vec2.x = mesh->mTextureCoords[0][i].x;
-                vec2.y = mesh->mTextureCoords[0][i].y;
-                vertex.TexCoords = vec2;
-            }
-            else
-            {
                 vertex.TexCoords = glm::vec2(0.0f, 0.0f);
-            }
 
             vertices.push_back(vertex);
         }
-
-        // Walk through each face (triangle) and get indices
+        // now wak through each of the mesh's faces (a face is a mesh its triangle) and retrieve the corresponding vertex indices.
         for (unsigned int i = 0; i < mesh->mNumFaces; i++)
         {
             aiFace face = mesh->mFaces[i];
+            // retrieve all indices of the face and store them in the indices vector
             for (unsigned int j = 0; j < face.mNumIndices; j++)
-            {
                 indices.push_back(face.mIndices[j]);
-            }
         }
-
-        // Process materials
+        // process materials
         aiMaterial *material = scene->mMaterials[mesh->mMaterialIndex];
+        // we assume a convention for sampler names in the shaders. Each diffuse texture should be named
+        // as 'texture_diffuseN' where N is a sequential number ranging from 1 to MAX_SAMPLER_NUMBER.
+        // Same applies to other texture as the following list summarizes:
+        // diffuse: texture_diffuseN
+        // specular: texture_specularN
+        // normal: texture_normalN
 
-        // 1. Diffuse maps
+        // 1. diffuse maps
         vector<Texture> diffuseMaps = loadMaterialTextures(material, aiTextureType_DIFFUSE, "texture_diffuse");
         textures.insert(textures.end(), diffuseMaps.begin(), diffuseMaps.end());
-
-        // 2. Specular maps
+        // 2. specular maps
         vector<Texture> specularMaps = loadMaterialTextures(material, aiTextureType_SPECULAR, "texture_specular");
         textures.insert(textures.end(), specularMaps.begin(), specularMaps.end());
-
-        // 3. Normal maps
-        vector<Texture> normalMaps = loadMaterialTextures(material, aiTextureType_HEIGHT, "texture_normal");
+        // 3. normal maps
+        std::vector<Texture> normalMaps = loadMaterialTextures(material, aiTextureType_HEIGHT, "texture_normal");
         textures.insert(textures.end(), normalMaps.begin(), normalMaps.end());
-
-        // 4. Height maps
-        vector<Texture> heightMaps = loadMaterialTextures(material, aiTextureType_AMBIENT, "texture_height");
+        // 4. height maps
+        std::vector<Texture> heightMaps = loadMaterialTextures(material, aiTextureType_AMBIENT, "texture_height");
         textures.insert(textures.end(), heightMaps.begin(), heightMaps.end());
 
-        // Return a Mesh object
+        // return a mesh object created from the extracted mesh data
         return Mesh(vertices, indices, textures);
     }
 
-    // Loads textures of a specific type for a given material
-    vector<Texture> loadMaterialTextures(aiMaterial *mat, aiTextureType type, const string &typeName)
+    vector<Texture> loadMaterialTextures(aiMaterial *mat, aiTextureType type, string typeName)
     {
         vector<Texture> textures;
         for (unsigned int i = 0; i < mat->GetTextureCount(type); i++)
         {
             aiString str;
             mat->GetTexture(type, i, &str);
-
-            // Check if we have already loaded this texture
-            if (textureCache.find(str.C_Str()) != textureCache.end())
+            bool skip = false;
+            for (unsigned int j = 0; j < textures_loaded.size(); j++)
             {
-                // Already loaded
-                textures.push_back(textureCache[str.C_Str()]);
+                if (std::strcmp(textures_loaded[j].path.data(), str.C_Str()) == 0)
+                {
+                    textures.push_back(textures_loaded[j]);
+                    skip = true;
+                    break;
+                }
             }
-            else
-            {
-                // Load a new texture
+            if (!skip)
+            { // if texture hasn't been loaded already, load it
                 Texture texture;
-                texture.id   = TextureFromFile(str.C_Str(), directory);
+                texture.id = TextureFromFile(str.C_Str(), directory);
                 texture.type = typeName;
                 texture.path = str.C_Str();
-
                 textures.push_back(texture);
-                textureCache[str.C_Str()] = texture;
+                textures_loaded.push_back(texture);
             }
         }
         return textures;
     }
 
-    // Helper to load texture from file using stb_image
     unsigned int TextureFromFile(const char *path, const string &directory)
     {
         string filename = string(path);
@@ -267,14 +196,11 @@ private:
                 format = GL_RGB;
             else if (nrComponents == 4)
                 format = GL_RGBA;
-            else
-                format = GL_RGB; // fallback
 
             glBindTexture(GL_TEXTURE_2D, textureID);
             glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
             glGenerateMipmap(GL_TEXTURE_2D);
 
-            // Set texture wrapping/filtering options
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
@@ -284,13 +210,10 @@ private:
         }
         else
         {
-            std::cout << "Texture failed to load at path: " << filename << std::endl;
+            std::cout << "Texture failed to load at path: " << path << std::endl;
             stbi_image_free(data);
         }
 
         return textureID;
     }
 };
-
-// Initialize the static texture cache
-std::unordered_map<std::string, Texture> Model::textureCache = {};
